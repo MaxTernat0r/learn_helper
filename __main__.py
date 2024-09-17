11accf2967a851dac4ceddb7b58f2f743e258ddd
MaxTernat0r <alabinmaxim@mail.ru>
2024-09-17 20:15:22 +0300

    Added function for see description if user want it

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ import csv
 terminology = []
 description = []
 errors_counter = 0
+unique_errors = 0
 
 with open("database/(17.09.2024)_foods.csv", encoding='utf-8') as r_file:
     file_reader = csv.reader(r_file, delimiter=';')
@@ -10,11 +11,19 @@ with open("database/(17.09.2024)_foods.csv", encoding='utf-8') as r_file:
         terminology.append(str(row[1]))
         description.append(str(row[0]))
 
+print(f"Total {len(terminology)} terms. If you don't know term, write '0' to see description. Let's start!")
 for i in range(len(terminology)):
     print(terminology[i])
     user_input = str(input())
     while user_input != description[i]:
-        print("Incorrect :( Try again...")
-        user_input = str(input())
+        if user_input == "0":
+            print(description[i])
+            print("Now type it")
+            user_input = str(input())
+        else:
+            print("Incorrect :( Try again...")
+            user_input = str(input())
+            errors_counter += 1
     else:
         print("Correct!")
+print(f"Final! Total errors: {errors_counter}")
