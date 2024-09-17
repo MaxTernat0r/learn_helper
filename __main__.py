64226edbae0500b014743381cf6dd2bf592f419a
MaxTernat0r <alabinmaxim@mail.ru>
2024-09-17 13:07:18 +0300

    food_words_added

--- __main__.py
+++ __main__.py
@@ -7,8 +7,8 @@ errors_counter = 0
 with open("database/(17.09.2024)_foods.csv", encoding='utf-8') as r_file:
     file_reader = csv.reader(r_file, delimiter=';')
     for row in file_reader:
-        terminology.append(str(row[0]))
-        description.append(str(row[1]))
+        terminology.append(str(row[1]))
+        description.append(str(row[0]))
 
 for i in range(len(terminology)):
     print(terminology[i])
